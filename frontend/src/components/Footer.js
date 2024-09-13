037fcf7db7ccbfea0f6445e0fd9ece714ed4511b
Adam <154867277+adamrachman-dev@users.noreply.github.com>
2024-09-13 14:17:44 +0700

    Change text to darker color

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className='flex justify-center items-center bg-nblue1 py-1'>
-            <p className='text-nblack1 text-sm'>&copy; 2024 Smart Web BTQ. All rights reserved.</p>
+            <p className='text-nblack4 text-sm'>&copy; 2024 Smart Web BTQ. All rights reserved.</p>
         </footer>
     );
 };
