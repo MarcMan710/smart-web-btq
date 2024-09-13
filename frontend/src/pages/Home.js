b8ff1c3414dabf84db3e152ef32e8bf7743d13ea
Adam <154867277+adamrachman-dev@users.noreply.github.com>
2024-09-13 14:23:14 +0700

    Add style to Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,18 +5,21 @@ import Footer from '../components/Footer'; // Tambahkan impor untuk Footer
 
 const Home = () => {
     return (
-        <div>
-            <header>
-                <h1>Welcome to Smart Web BTQ</h1>
-                <p>Your one-stop solution for personalized learning and progression tracking.</p>
-                <div>
-                    <Link to="/register">Get Started</Link>
-                    <span> or </span>
-                    <Link to="/login">Login</Link>
+        <div className='flex flex-col space-y-12 items-center'>
+            {/* hero */}
+            <header className='flex flex-col items-center my-20'>
+                <h1 className='text-4xl font-bold text-nblack4'>Welcome to Smart Web BTQ</h1>
+                <p className='text-sm text-nblack1 mb-4'>Your one-stop solution for personalized learning and progression tracking.</p>
+                <div className='font-bold text-nblue4'>
+                    <Link className='hover:text-nblue3' to="/register">Get Started</Link>
+                    <span className='font-normal text-nblack4'> or </span>
+                    <Link className='hover:text-nblue3' to="/login">Login</Link>
                 </div>
             </header>
-            <section>
-                <img src="path/to/your/banner/image.jpg" alt="App Banner" style={{ width: '100%', height: 'auto' }} />
+
+            {/* Temporary app banner */}
+            <section className='h-[240px] w-full bg-gradient-to-r from-nblue1 to-nblue4'>
+                {/* <img src="path/to/your/banner/image.jpg" alt="App Banner" style={{ width: '100%', height: 'auto' }} /> */}
             </section>
             {/* <Footer /> */}{/* Tambahkan Footer di sini */}
         </div>
