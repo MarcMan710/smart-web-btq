d1f806c37aa28c7cb7114ce3075f571a06813b01
Adam <154867277+adamrachman-dev@users.noreply.github.com>
2024-09-16 08:48:59 +0700

    Add style to SubmitRecording.js

--- frontend/src/pages/SubmitRecording.js
+++ frontend/src/pages/SubmitRecording.js
@@ -20,17 +20,26 @@ const SubmitRecording = () => {
     };
 
     return (
-        <div>
-            <h1>Proses Rekaman</h1>
-            <p>Ikuti panduan visual dan audio berikut untuk melakukan rekaman...</p>
+        <div className='flex flex-col items-center px-6 py-32 text-nblack4'>
+            <h1 className='font-bold text-4xl'>Proses Rekaman</h1>
+            <p className='mb-4'>Masukan url rekaman untuk mengirim.</p>
             <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    value={audioUrl}
-                    onChange={(e) => setAudioUrl(e.target.value)}
-                    placeholder="Enter audio URL"
-                />
-                <button type="submit">Submit</button>
+                <div className='flex flex-col items-center space-y-4'>
+                    {/* Logo */}
+                    <div className='p-6 bg-nwhite2 shadow-sm'>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-10">
+                            <path fill-rule="evenodd" d="M19.902 4.098a3.75 3.75 0 0 0-5.304 0l-4.5 4.5a3.75 3.75 0 0 0 1.035 6.037.75.75 0 0 1-.646 1.353 5.25 5.25 0 0 1-1.449-8.45l4.5-4.5a5.25 5.25 0 1 1 7.424 7.424l-1.757 1.757a.75.75 0 1 1-1.06-1.06l1.757-1.757a3.75 3.75 0 0 0 0-5.304Zm-7.389 4.267a.75.75 0 0 1 1-.353 5.25 5.25 0 0 1 1.449 8.45l-4.5 4.5a5.25 5.25 0 1 1-7.424-7.424l1.757-1.757a.75.75 0 1 1 1.06 1.06l-1.757 1.757a3.75 3.75 0 1 0 5.304 5.304l4.5-4.5a3.75 3.75 0 0 0-1.035-6.037.75.75 0 0 1-.354-1Z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <input
+                        type="text"
+                        value={audioUrl}
+                        onChange={(e) => setAudioUrl(e.target.value)}
+                        placeholder="Enter audio URL"
+                        className='px-2 py-1 rounded-md'
+                    />
+                    <button className='font-bold px-6 py-1 text-nwhite1 bg-nblue4 rounded-full hover:bg-nblue3' type="submit">Submit</button>
+                </div>
             </form>
         </div>
     );
