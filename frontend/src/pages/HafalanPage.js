e480cfc753a74b547a1e124e5ed471b56f7551c9
Adam Marsa Rachman <154867277+adamrachman-dev@users.noreply.github.com>
2024-10-14 15:42:20 +0700

    Add dummy hafalan card

--- frontend/src/pages/HafalanPage.js
+++ frontend/src/pages/HafalanPage.js
@@ -28,9 +28,26 @@ const HafalanPage = () => {
         <div className='flex flex-col items-center px-6 py-32 text-nblack4'>
             <h1 className='font-bold text-4xl mb-4'>Halaman Hafalan</h1>
             <div className="flex flex-col space-y-6">
+                {/* Display card using map */}
+                {/* 
                 {hafalanList.map(hafalan => (
                     <HafalanCard key={hafalan._id} hafalan={hafalan} onClick={() => handleCardClick(hafalan)} />
                 ))}
+                 */}
+
+                {/* Temporary dummy */}
+                <div className="bg-nwhite2 shadow-md p-6 w-[45ch] cursor-pointer" onClick={onClick}>
+                    <h2>Surah Al-Fatihah</h2>
+                    <p>Level: 1</p>
+                </div>
+                <div className="bg-nwhite2 shadow-md p-6 w-[45ch] cursor-pointer" onClick={onClick}>
+                    <h2>Surah Al-Insyirah</h2>
+                    <p>Level: 2</p>
+                </div>
+                <div className="bg-nwhite2 shadow-md p-6 w-[45ch] cursor-pointer" onClick={onClick}>
+                    <h2>Surah Al-Ikhlas</h2>
+                    <p>Level: 1</p>
+                </div>
             </div>
             {selectedHafalan && (
                 <HafalanModal hafalan={selectedHafalan} onClose={handleCloseModal} />
