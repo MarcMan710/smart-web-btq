f008ee5739d132d4aea16bf67dc40a082c1a6d67
Adam Marsa Rachman <154867277+adamrachman-dev@users.noreply.github.com>
2024-10-27 15:12:28 +0700

    Adjust not found page styling

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,16 +1,19 @@
 // frontend/src/pages/NotFound.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Button from '../components/Button';
 
 const NotFound = () => {
     return (
-        <div className='flex flex-col items-center px-6 py-44 text-nblack4'>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-12 text-nred">
+        <div className='flex flex-col items-center text-nblack4'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-16 text-nyellow">
                 <path fillRule="evenodd" d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clipRule="evenodd" />
             </svg>
-            <h2 className='font-bold text-4xl mb-4'>Page Not Found</h2>
-            <p>The page you are looking for does not exist.</p>
-            <Link className='font-semibold text-nblue4 hover:text-nblue3' to="/">Go Back Home</Link>
+            <h1 className='font-bold text-4xl mb-2'>Halaman Tidak Ditemukan</h1>
+            <p className='mb-4 text-sm'>Halaman yang kamu cari tidak ada</p>
+            <Link to="/dashboard">
+                <Button>Kembali</Button>
+            </Link>
         </div>
     );
 };
