91f6018194f132aa4fdf75fc46303141744c4ae5
Adam Marsa Rachman <154867277+adamrachman-dev@users.noreply.github.com>
2024-10-27 15:36:13 +0700

    Add props to button component

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,8 +1,16 @@
 // frontend/src/components/Button.js
-const Button = ({ children, onClick, type = "button" }) => {
+const Button = ({
+    children,
+    onClick,
+    text = "text-nwhite1",
+    bg = "bg-nblue4",
+    hover = "hover:bg-nblue3",
+    type = "button"
+}) => {
     return (
         <button
-            className="w-[16ch] font-bold text-nwhite1 bg-nblue4 py-2 rounded-lg hover:bg-nblue3"
+            className={`w-[16ch] font-bold py-2 rounded-lg ${text} ${bg} ${hover}`}
+            // className="w-[16ch] font-bold text-nwhite1 bg-nblue4 py-2 rounded-lg hover:bg-nblue3"
             type={type}
             onClick={onClick}
         >
