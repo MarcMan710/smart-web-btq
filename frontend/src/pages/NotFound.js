433a758311cfec565c58e47cef79fd26369b7bcc
Adam Marsa Rachman <154867277+adamrachman-dev@users.noreply.github.com>
2024-10-27 16:36:22 +0700

    Adjust not found page styling

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -6,13 +6,15 @@ import Button from '../components/Button';
 const NotFound = () => {
     return (
         <div className='flex flex-col items-center text-nblack4'>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-16 text-nyellow">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-12 text-nred mb-4">
                 <path fillRule="evenodd" d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clipRule="evenodd" />
             </svg>
             <h1 className='font-bold text-4xl mb-2'>Halaman Tidak Ditemukan</h1>
             <p className='mb-4 text-sm'>Halaman yang kamu cari tidak ada</p>
             <Link to="/dashboard">
-                <Button>Kembali</Button>
+                <Button text='text-nblack4' bg='bg-nwhite2' hover='hover:bg-nwhite3'>
+                    Kembali
+                </Button>
             </Link>
         </div>
     );
