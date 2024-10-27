06b9c1f8fb483c9630dc51d29393b55e638e00f8
Adam Marsa Rachman <154867277+adamrachman-dev@users.noreply.github.com>
2024-10-27 15:53:35 +0700

    Adjust instruction page layout

--- frontend/src/pages/InstructionPage.js
+++ frontend/src/pages/InstructionPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import Button from '../components/Button';
 
 const InstructionPage = () => {
     const navigate = useNavigate();
@@ -9,25 +10,25 @@ const InstructionPage = () => {
     };
 
     return (
-        <div className='flex flex-col items-center px-6 py-32 text-nblack4'>
-            <h1 className='font-bold text-4xl'>Instruksi Rekaman Hafalan</h1>
-            <p className='mb-4'>Ikuti instruksi berikut untuk melakukan rekaman hafalan.</p>
-            {/* Step by step */}
+        <div className='flex flex-col items-center text-nblack4'>
+            <h1 className='font-bold text-4xl mb-2'>Instruksi</h1>
+            <p className='text-sm mb-4'>Ikuti instruksi berikut untuk melakukan rekaman hafalan</p>
+
             <div className='flex flex-col w-[45ch] space-y-2 mb-4'>
                 <div className='flex space-x-2 items-center'>
                     <div className='font-bold text-nwhite1 bg-nblue4 rounded-full py-1 px-4'>1</div>
-                    <p>Instruksi 1: Mulai rekaman dengan menekan tombol "Mulai"</p>
+                    <p>Mulai rekaman dengan menekan tombol <span className='font-bold'>Mulai</span></p>
                 </div>
                 <div className='flex space-x-2 items-center'>
                     <div className='font-bold text-nwhite1 bg-nblue4 rounded-full py-1 px-4'>2</div>
-                    <p>Instruksi 2: Hentikan rekaman dengan menekan tombol "Hentikan"</p>
+                    <p>Hentikan rekaman dengan menekan tombol <span className='font-bold'>Hentikan</span></p>
                 </div>
                 <div className='flex space-x-2 items-center'>
                     <div className='font-bold text-nwhite1 bg-nblue4 rounded-full py-1 px-4'>3</div>
-                    <p>Instruksi 3: Submit rekaman dengan menekan tombol "Submit"</p>
+                    <p>Submit rekaman dengan menekan tombol <span className='font-bold'>Submit</span></p>
                 </div>
             </div>
-            <button className='font-bold text-nwhite1 bg-nblue4 rounded-full px-6 py-1 hover:bg-nblue3' onClick={handleStart}>Mulai</button>
+            <Button onClick={handleStart}>Mulai</Button>
         </div>
     );
 };
