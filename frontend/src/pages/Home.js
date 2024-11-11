3a185d47cb96b3511c1acdb99158ce5e35e6c870
Adam Marsa Rachman <154867277+adamrachman-dev@users.noreply.github.com>
2024-11-11 07:00:35 +0700

    Change wording on banner

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -64,10 +64,11 @@ const Home = () => {
             </svg>
           </div>
           <p className="font-bold mb-2">
-            <i>Artificial Intelligent</i>
+            <i>Speech Recognition</i>
           </p>
           <p className="text-sm text-center text-nblack1">
-            Aplikasi menggunakan model AI untuk memberikan penilaian yang sesuai
+            Aplikasi menggunakan model AI untuk penilaian otomatis kepada
+            hafalan
           </p>
         </div>
 
