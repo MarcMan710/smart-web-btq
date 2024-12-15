c364fcf75af8edd765552ba891bc0a96908c3de9
Adam Marsa Rachman <154867277+adamrachman-dev@users.noreply.github.com>
2024-12-15 20:28:02 +0700

    Adjust dashboard layout

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -91,21 +91,15 @@ const Dashboard = () => {
           <p className="text-sm">
             {day} {monthName}, {year}
           </p>
-          <div className="flex space-x-1 mb-4">
+          <div className="flex space-x-1 mb-4 items-center">
             <p className="font-bold text-xl">Assalamualaikum, {firstName}</p>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              fill="none"
               viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
+              fill="currentColor"
+              className="size-6 text-nyellow"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M10.05 4.575a1.575 1.575 0 1 0-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 0 1 3.15 0v1.5m-3.15 0 .075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 0 1 3.15 0V15M6.9 7.575a1.575 1.575 0 1 0-3.15 0v8.175a6.75 6.75 0 0 0 6.75 6.75h2.018a5.25 5.25 0 0 0 3.712-1.538l1.732-1.732a5.25 5.25 0 0 0 1.538-3.712l.003-2.024a.668.668 0 0 1 .198-.471 1.575 1.575 0 1 0-2.228-2.228 3.818 3.818 0 0 0-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0 1 16.35 15m.002 0h-.002"
-              />
+              <path d="M10.5 1.875a1.125 1.125 0 0 1 2.25 0v8.219c.517.162 1.02.382 1.5.659V3.375a1.125 1.125 0 0 1 2.25 0v10.937a4.505 4.505 0 0 0-3.25 2.373 8.963 8.963 0 0 1 4-.935A.75.75 0 0 0 18 15v-2.266a3.368 3.368 0 0 1 .988-2.37 1.125 1.125 0 0 1 1.591 1.59 1.118 1.118 0 0 0-.329.79v3.006h-.005a6 6 0 0 1-1.752 4.007l-1.736 1.736a6 6 0 0 1-4.242 1.757H10.5a7.5 7.5 0 0 1-7.5-7.5V6.375a1.125 1.125 0 0 1 2.25 0v5.519c.46-.452.965-.832 1.5-1.141V3.375a1.125 1.125 0 0 1 2.25 0v6.526c.495-.1.997-.151 1.5-.151V1.875Z" />
             </svg>
           </div>
           <p className="text-sm mb-1">Tugas kamu</p>
@@ -128,7 +122,7 @@ const Dashboard = () => {
         <img src={potrait} className="w-[8rem]"></img>
       </div>
 
-      <div className="flex flex-col bg-nblue1/30 p-6 rounded-lg min-w-[75ch] mb-4">
+      <div className="flex flex-col bg-gradient-to-b from-nblue1/30 to-nblue1/70 p-6 rounded-lg min-w-[75ch] mb-4">
         <p className="font-bold text-nblack4 text-lg mb-4">Tugas</p>
         <div className="flex space-x-4 items-start justify-between">
           <div className="flex flex-1 flex-col p-4 rounded-lg bg-nwhite1">
@@ -188,7 +182,7 @@ const Dashboard = () => {
         </div>
       </div>
 
-      <div className="flex flex-col bg-npurple/30 p-6 rounded-lg w-[75ch] mb-20">
+      <div className="flex flex-col bg-gradient-to-b from-npurple/30 to-npurple/70 p-6 rounded-lg w-[75ch] mb-20">
         <p className="font-bold text-nblack4 text-lg mb-4">Materi</p>
 
         <div
